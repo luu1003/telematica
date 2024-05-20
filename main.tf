15bdca96d7e0f62ecce3bc91aa4a7eb27f7f4f2f
luu1003 <cadavid.mmaria.l@gmail.com>
2024-05-19 23:36:23 -0500

    update

--- main.tf
+++ main.tf
@@ -37,6 +37,15 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -5,5 +5,3 @@ sudo yum install git -y
 sudo git clone https://github.com/luu1003/telematica.git
 sudo yum install -y docker
 sudo service docker start
-sudo docker build -f /telematica/Dockerfile -t web-image:v1 .
-sudo docker run -d -p 80:80 web-image:v1
