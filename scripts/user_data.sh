f166ecc305370ba1c2fb8f6ebee3bbd32fd92202
luu1003 <cadavid.mmaria.l@gmail.com>
2024-05-19 22:08:50 -0500

    Update user_data.sh

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -6,5 +6,5 @@ sudo git clone https://github.com/luu1003/telematica.git
 sudo cd telematica
 sudo yum install -y docker
 sudo service docker start
-docker build -t web-image:v1 .
-docker run -d -p 80:80 web-image:v1
+sudo docker build -t web-image:v1 .
+sudo docker run -d -p 80:80 web-image:v1
