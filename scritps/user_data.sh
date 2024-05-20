eb43a54429bc00cad5691f6bb1502c8a40bde1be
luu1003 <cadavid.mmaria.l@gmail.com>
2024-05-19 21:35:20 -0500

    Create user_data.sh

---
+++ scritps/user_data.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo yum update -y
+sudo yum install git -y
+sudo git clone https://github.com/luu1003/telematica.git
+sudo yum install -y docker
+sudo service docker start
+docker build -t web-image:v1 .
+docker run -d -p 80:80 web-image:v1
