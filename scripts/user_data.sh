a8432984ee074f8911d536b15cb117ee966c0545
luu1003 <cadavid.mmaria.l@gmail.com>
2024-05-19 21:58:06 -0500

    fix issue

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -3,6 +3,7 @@
 sudo yum update -y
 sudo yum install git -y
 sudo git clone https://github.com/luu1003/telematica.git
+sudo cd telematica
 sudo yum install -y docker
 sudo service docker start
 docker build -t web-image:v1 .
